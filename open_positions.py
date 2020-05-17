a7598a63c11a88f7d78da1f7bcb2766f2b63b084
leesanduo <60281039+leesanduo@users.noreply.github.com>
2020-05-18 02:03:53 +0800

    Showing exposures in USD. Required input: updated live rates ag USD
    Visualize MTM and distance to SL and TP

--- open_positions.py
+++ open_positions.py
@@ -1,8 +1,7 @@
 '''To quickly see what open positions I currently have - using my xls file "TradingJournal (FX) (2020).xlsx '''
 
 import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
+import numpy as np
 
 df = pd.read_excel(r"/Users/sanduo/Documents/Trading/TradingJournal (FX) (2020).xlsx", \
                    sheet_name = 'Trade Log', \
@@ -16,7 +15,7 @@ df_alltrades = df[~filter]
 # Using the Price Out column to identify which trades I have not closed yet, i.e. Open Positions
 open_filter = df_alltrades['Price Out'].isna()
 open_positions = df_alltrades[open_filter]
-pd.options.display.float_format = '{:,.2f}'.format # Format with commas and round off to 2dp
+pd.options.display.float_format = '{:,.4f}'.format # Format with commas and round off to 2dp
 
 open_ccys = open_positions[['L/S','Notional','Pair', 'Price In', 'SL', 'TP']]
 print(open_ccys)
@@ -61,7 +60,41 @@ ax.yaxis.set_major_formatter(tick)
 # Bar plot
 sns.barplot(x='currency', y='exposure(usd)', data=exposures_sorted, palette="rocket", ax=ax)
 
-ax.set_title('Exposures (expressed in USD) \n -{}-'.format(datetime.now().strftime("%d/%m/%Y %H:%M")))
+ax.set_title('Exposures (expressed in USD)\n-{}-'.format(datetime.now().strftime("%d/%m/%Y %H:%M")))
 ax.set_ylabel('')
 ax.set_xlabel('')
+
+
+### Visualize MTM and distance to SL and TP
+
+# Vlookup / Merge usd_rates_df with open_ccys twice, based on base_ccy and pricing_ccy
+base_live = pd.merge(open_ccys, usd_rates_df, left_on='base_ccy', right_on='xxxusd', how='left')
+open_ccys_live = pd.merge(base_live, usd_rates_df, left_on='pricing_ccy', right_on='xxxusd', suffixes=['_base','_pricing'], how='left')
+
+# Get current price 'last' - rates_base/rates_pricing
+open_ccys_live['last'] = open_ccys_live['rates_base']/open_ccys_live['rates_pricing']
+
+# Calculate pnl % gain (+) or loss (-)
+open_ccys_live['chg_pct']=((open_ccys_live['last']/open_ccys_live['Price In'])-1)*100
+open_ccys_live['sl_pct']=((open_ccys_live['SL']/open_ccys_live['Price In'])-1)*100
+open_ccys_live['tp_pct']=((open_ccys_live['TP']/open_ccys_live['Price In'])-1)*100
+
+# Plot pnl_pct, on barh plot
+fig2, ax2 = plt.subplots(figsize=(10,5))
+
+sns.barplot(x='sl_pct', y='Pair', data=open_ccys_live, label='SL', color='red', alpha=0.3, ax=ax2)
+sns.barplot(x='tp_pct', y='Pair', data=open_ccys_live, label='TP', color='green', alpha=0.3, ax=ax2)
+
+open_ccys_live.loc[(open_ccys_live['chg_pct'] >= 0) & (open_ccys_live['L/S']=='L'), 'positive_pnl'] = open_ccys_live['chg_pct']
+open_ccys_live.loc[(open_ccys_live['chg_pct'] < 0) & (open_ccys_live['L/S']=='S'), 'positive_pnl'] = open_ccys_live['chg_pct']
+open_ccys_live.loc[(open_ccys_live['chg_pct'] >= 0) & (open_ccys_live['L/S']=='S'), 'negative_pnl'] = open_ccys_live['chg_pct']
+open_ccys_live.loc[(open_ccys_live['chg_pct'] < 0) & (open_ccys_live['L/S']=='L'), 'negative_pnl'] = open_ccys_live['chg_pct']
+
+sns.barplot(x='positive_pnl', y='Pair', data=open_ccys_live, label='MTM', color='green', ax=ax2)
+sns.barplot(x='negative_pnl', y='Pair', data=open_ccys_live, label='MTM', color='red', ax=ax2)
+
+ax2.set_title('Marked to Market\n-{}-'.format(datetime.now().strftime("%d/%m/%Y %H:%M")))
+ax2.set_ylabel('CCY Pair')
+ax2.set_xlabel('% PnL')
+ax2.xaxis.set_major_formatter(mtick.PercentFormatter())
 plt.show()
